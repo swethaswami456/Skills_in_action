19fac9f070a85117f4ff0577ec74df12ce25ded7
swethaswami456 <swethaswami456@gmail.com>
2026-08-08 22:26:22 +0530

    Created new orphan branch dental pages

---
+++ script.js
@@ -0,0 +1,7 @@
+function Online_Appointment() {
+    alert("Available by appointment and offline direct visit to clinic during open hours");
+}
+
+function In_person_Appointment() {
+    alert("Please call us at 📞 98765 43210");
+}
